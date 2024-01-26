4a91df51b1abb393ad204a14b210b2459efb090a
cosmintianu <cosmin.tianunicolae@yahoo.com>
2024-01-26 23:52:19 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from matplotlib import pyplot as plt #image display
+
+data = pd.read_csv('data/train.csv')
+
+#data.head()
