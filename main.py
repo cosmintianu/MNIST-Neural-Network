14eca8b8d6f8441f5522a6a78b51b9374b9879a5
cosmintianu <cosmin.tianunicolae@yahoo.com>
2024-01-27 00:06:28 +0200

    Added training

--- main.py
+++ main.py
@@ -4,4 +4,103 @@ from matplotlib import pyplot as plt #image display
 
 data = pd.read_csv('data/train.csv')
 
-#data.head()
+#print(data.head(10))
+#print(data.head())
+#print(data)
+
+data = np.array(data)
+m,n = data.shape
+np.random.shuffle(data)
+
+data_dev = data[0:1000].T
+Y_dev = data_dev[0]
+X_dev = data_dev[1:n]
+X_dev = X_dev / 255.
+
+data_train = data[1000:m].T
+Y_train = data_train[0]
+X_train = data_train[1:n]
+X_train = X_train / 255.
+
+#print(X_train[:,0].shape) #print nr of imputs
+
+def init_params(size):
+    W1 = np.random.rand(10, size) - 0.5
+    b1 = np.random.rand(10, 1) - 0.5
+    W2 = np.random.rand(10, 10) - 0.5
+    b2 = np.random.rand(10, 1) - 0.5
+
+    return W1, b1, W2, b2
+
+
+def ReLU(Z):
+    return np.maximum(0, Z)
+
+
+def softmax(Z):
+    Z -= np.max(Z, axis=0)  # Subtract max value for numerical stability
+    A = np.exp(Z) / np.sum(np.exp(Z), axis=0)
+    return A
+
+
+def forward_prop(W1, b1, W2, b2, X):
+    Z1 = W1.dot(X) + b1
+    A1 = ReLU(Z1)
+
+    Z2 = W2.dot(A1) + b2
+    A2 = softmax(Z2)
+    return Z1, A1, Z2, A2
+
+
+def one_hot(Y):  # return a column vector with 1 on the output of the forward_prop
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
+
+
+def ReLU_deriv(Z):
+    return Z > 0  # derivative of ReLU
+
+
+def back_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
+    one_hot_Y = one_hot(Y)
+    dZ2 = A2 - one_hot_Y
+    dW2 = 1 / m * dZ2.dot(A1.T)
+    db2 = 1 / m * np.sum(dZ2)
+    dZ1 = W2.T.dot(dZ2) * ReLU_deriv(Z1)
+    dW1 = 1 / m * dZ1.dot(X.T)
+    db1 = 1 / m * np.sum(dZ1)
+    return dW1, db1, dW2, db2
+
+
+def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1
+    W2 = W2 - alpha * dW2
+    b2 = b2 - alpha * db2
+    return W1, b1, W2, b2
+
+def get_predictions(A2):
+    return np.argmax(A2, 0)
+
+def get_accuracy(predictions, Y):
+    return np.sum(predictions == Y)/Y.size
+
+def gradient_descent(X, Y, alpha, iterations):
+    size , m = X.shape
+
+    W1, b1, W2, b2 = init_params(size)
+    for i in range(iterations):
+        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = back_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
+
+        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+
+        if (i+1) % int(iterations/10) == 0:
+            print(f"Iteration: {i+1} / {iterations}")
+            prediction = get_predictions(A2)
+            print(f'{get_accuracy(prediction, Y):.3%}')
+    return W1, b1, W2, b2
+
+W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.15, 200)
